ac0036451206fe53dbbb56ce7d06cd2dd8f00607
Gabriel Magioli <gabrielmagioli1@gmail.com>
2023-12-21 16:51:04 -0300

    .

--- src/pages/jogo/Jogo.tsx
+++ src/pages/jogo/Jogo.tsx
@@ -30,11 +30,104 @@ function Jogo(){//funcao que possui a tela principal -> jogo da velha(tabuleiro)
     const [simboloAtual, setSimboloAtual]=useState<string>('X')
     const [jogando,setJogando]=useState<boolean>(true)
 
+    const tabuleiro = (j) => {
+        return(
+            <div className={styles.tabuLinha}>
+                <div className={styles.casa} data-pos='00' onClick="">{jogo[0][0]}</div>
+                <div className={styles.casa} data-pos='01' onClick="">{jogo[0][1]}</div>
+                <div className={styles.casa} data-pos='02' onClick="">{jogo[0][2]}</div>
+            </div>
+            <div className={styles.tabuLinha}>
+                <div className={styles.casa} data-pos='10' onClick="">{jogo[1][0]}</div>
+                <div className={styles.casa} data-pos='11' onClick="">{jogo[1][1]}</div>
+                <div className={styles.casa} data-pos='12' onClick="">{jogo[1][2]}</div>
+            </div>
+            <div className={styles.tabuLinha}>
+                <div className={styles.casa} data-pos='20' onClick="">{jogo[2][0]}</div>
+                <div className={styles.casa} data-pos='21' onClick="">{jogo[2][1]}</div>
+                <div className={styles.casa} data-pos='22' onClick="">{j[2][2]}</div>
+            </div>
+        )
+        
+    }
+
+    const verificaVitoria=()=>{
+        //linhas
+        let pontos:number =0
+        let vitoria: boolean =false
+        for(let l=0;l<3;l++){
+            pontos=0;
+            for(let c=0;c<3;c++){
+                if(jogo[l][c]==simboloAtual){
+                    pontos++
+                }
+            }
+            if(pontos>=3){
+                vitoria=true
+                break
+            }
+        }
+
+        //colunas
+        for(let c=0;c<3;c++){
+            pontos=0
+            for(let l=0;l<3;l++){
+                if(jogo[l][c]==simboloAtual){
+                    pontos++
+                }
+            }
+            if(pontos>=3){
+                vitoria=true
+                break
+            }
+        }
+
+        //diagonais
+        pontos=0
+        for(let d=0;d<3;d++){
+            if(jogo[d][d]){
+                pontos++
+            }
+        }
+        if(pontos>=3){
+            vitoria=true;
+        }   
+        pontos=0
+        let l:number =0
+        for(let c=2;c>=0;c--){
+            if(jogo[l][c]==simboloAtual){
+                pontos++
+            }
+            l++
+        }
+        if(pontos>=3){
+            vitoria=true
+        }
+        return vitoria
+    }
+
+    const trocaJogador=()=>{
+        simboloAtual=='X'?setSimboloAtual('O'):setSimboloAtual('X')
+    }
+
+    const retPos=(e:any)=>{
+        const p=e.target.getAttribute('data-pos')
+        const pos:number[]=[parseInt(p.substring(0,1)), parseInt(p.substring(1,2))]
+        return pos
+    }
+
+    const verificaEspacoVazio=(e:any)=>{
+        if(jogo[retPos(e)[0]][retPos(e)[1]]==''){
+            return true
+        }else{
+            return false
+        }
+    }
    
 
-    const joga = (e) => {
-        if(jogando == true){
-            if(VerificaEspacoVazio(e)){
+    const joga = (e:any) => {
+        if(jogando){
+            if(verificaEspacoVazio(e)){
                 jogo[retPos(e)[0], retPos(e)[1]] = simboloAtual;
                 trocaJogador();
                 if(verificaVitoria()){
@@ -73,21 +166,7 @@ function Jogo(){//funcao que possui a tela principal -> jogo da velha(tabuleiro)
                     </div>
 
                     <div className={styles.jogo}>
-                        <div className={styles.tabuLinha}>
-                            <div className={styles.casa} data-pos='00' onClick="">{jogo[0][0]}</div>
-                            <div className={styles.casa} data-pos='01' onClick="">{jogo[0][1]}</div>
-                            <div className={styles.casa} data-pos='02' onClick="">{jogo[0][2]}</div>
-                        </div>
-                        <div className={styles.tabuLinha}>
-                            <div className={styles.casa} data-pos='10' onClick="">{jogo[1][0]}</div>
-                            <div className={styles.casa} data-pos='11' onClick="">{jogo[1][1]}</div>
-                            <div className={styles.casa} data-pos='12' onClick="">{jogo[1][2]}</div>
-                        </div>
-                        <div className={styles.tabuLinha}>
-                            <div className={styles.casa} data-pos='20' onClick="">{jogo[2][0]}</div>
-                            <div className={styles.casa} data-pos='21' onClick="">{jogo[2][1]}</div>
-                            <div className={styles.casa} data-pos='22' onClick="">{jogo[2][2]}</div>
-                        </div>
+                        
                     </div>
 
                     <div className={styles.pontuacao}>
